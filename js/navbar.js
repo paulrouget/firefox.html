9bec11f719e5704dc5e35e01617a89881449f1c0
Your Name <your@email.com>
2014-12-27 14:28:24 -0600

    fixed code style issues
    removed trailing whitespace

--- js/navbar.js
+++ js/navbar.js
@@ -29,7 +29,7 @@ function(UrlHelper, TabIframeDeck, RegisterKeyBindings) {
       <hbox class='urlbar' flex='1' align='center'>
         <div class='identity'></div>
         <input placeholder='Search or enter address' class='urlinput' flex='1'>
-        <span class='button-wrapper'>      
+        <span class='button-wrapper'>
           <button class='reload-button' title='Reload current page'></button>
           <button class='stop-button' title='Stop loading this page'></button>
         </span>
